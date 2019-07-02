74571b9c5e2af4d16b780deda1a37df77ed4ac91
claudiohpdev <junior.claudio1989@gmail.com>
2019-07-01 21:59:39 -0300

    Java Commit

---
+++ JavaCertification/src/com/br/eclipse/javaBasics/JavaBasics.java
@@ -0,0 +1,15 @@
+package com.br.eclipse.javaBasics;
+
+public class JavaBasics {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 20;
+		
+		int resultado = a + b;
+		
+		System.out.println(resultado);
+	
+	}
+}
